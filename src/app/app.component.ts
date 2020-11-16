787cd01ff8fa9383e2dddd9cd7897e1637548104
LeonardoSaenz <leonardorsaenz@gmail.com>
2020-11-16 15:29:49 -0600

    Yuyee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {CookieService} from 'ngx-cookie-service';
+import { freeApiService } from './services/freeapi.service'
+import { Comments } from './classes/comments'
 
 @Component({
   selector: 'app-root',
@@ -9,20 +11,51 @@ import {CookieService} from 'ngx-cookie-service';
 export class AppComponent {
   title = 'Tarea5';
 
-  constructor(private cookie: CookieService){}
+
+  constructor(private cookie: CookieService,
+              private _freeApiService:freeApiService,
+              
+  ){}
+
+  lstcomments:Comments[] | undefined
+  
+
+  ngOnInit()
+  {
+    this._freeApiService.getcomments()
+    .subscribe(
+      data =>
+      {
+        this.lstcomments = data;
+      }
+    )
+  }
+
   allMight(){
-    this.cookie.set("UserID","ABC")
+    this.cookie.set("Prueba","ABC")
   }
 
   allDeku()
   {
-    alert(this.cookie.get("UserID"))
+      alert("ID: "+this.cookie.get("UserId"))
+      alert("Email: "+this.cookie.get("UserEmail"))
+      alert("UserPostName: "+this.cookie.get("UserPostName"))
+      alert("Body: "+this.cookie.get("UserBody"))
   }
 
   allForOne()
   {
-    this.cookie.delete("UserID")
+    this.cookie.deleteAll()
   }
 
+  MidoriyaShonen(com:any)
+  {
+    this.cookie.set("UserEmail",com.email)
+    this.cookie.set("UserId",com.id)
+    this.cookie.set("UserPostName",com.name)
+    this.cookie.set("UserBody",com.body)
+  }
+
+  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { freeApiService } from './services/freeapi.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [CookieService],
+  imports: [BrowserModule,FormsModule,HttpClientModule],
+  providers: [CookieService,freeApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
